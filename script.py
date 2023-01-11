127a7c151ec4a6a292078cdd2408f443eba70117
Camilo Italo Oliveira Lima <italo@COSAIR>
2023-01-11 15:02:10 -0300

    Add Class Users

--- script.py
+++ script.py
@@ -6,3 +6,34 @@ from json import dumps
 db_connect = create_engine('sqlite:///exemplo.db')
 app = Flask(__name__)
 api = Api(app)
+
+class Users(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select * from user")
+        result = [dict(zip(tuple(query.keys()), i)) for i in query.cursor]
+        return jsonify(result)
+    
+    def post(self):
+        conn = db_connect.connect()
+        name = request.json['name']
+        email = request.json['email']
+
+        conn.execute(
+            "insert into user values(null, '{0}', '{1}')".format(name, email))
+
+        query = conn.execute('select * from user order by id desc limit 1')
+        result = [dict(zip(tuple(query.keys()),i)) for i in query.cursor]
+        return jsonify(result)
+
+    def put(self):
+        conn = db_connect.connect()
+        id = request.json['id']
+        name = request.json['name']
+        email = request.json['email']
+
+        conn.execute("update user set name ='" + str(name) + 
+        "',email ='" + str(email) +"' where id =%d " % int(id))
+        query = conn.execute("select * from user where id =%d " % int(id))
+        result = [dict(zip(tuple(query.keys()),i)) for i in query.cursor]
+        return jsonify(result)
