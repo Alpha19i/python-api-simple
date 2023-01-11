374cca8bcda2b2896cc2664871cef0b08d4efde5
Camilo Italo Oliveira Lima <italo@COSAIR>
2023-01-11 15:01:28 -0300

    Add DB and Use Flask

--- script.py
+++ script.py
@@ -2,3 +2,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import *
 from json import dumps
+
+db_connect = create_engine('sqlite:///exemplo.db')
+app = Flask(__name__)
+api = Api(app)
